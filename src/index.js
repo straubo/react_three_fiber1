7dae8b6c9dedbbe7700abade89fb0c73919d7fab
Casey Berman <caseybermanprograms@gmail.com>
2022-04-25 17:31:00 -0400

    refactored ocean.js

--- src/index.js
+++ src/index.js
@@ -7,36 +7,7 @@ import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fibe
 import * as THREE from 'three';
 import { OrbitControls } from '@react-three/drei';
 import waterNormalsJPEG from './waternormals.jpg';
-
-// water
-import { Water } from 'three-stdlib'
-extend({ Water })
-
-function Ocean() {
-  const ref = useRef()
-  const gl = useThree((state) => state.gl)
-  const waterNormals = useLoader(THREE.TextureLoader, waterNormalsJPEG);
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
-  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
-  const config = useMemo(
-    () => ({
-      textureWidth: 512,
-      textureHeight: 512,
-      waterNormals,
-      sunDirection: new THREE.Vector3(),
-      sunColor: 0xffffff,
-      // waterColor: 0x001e0f,
-      waterColor: 0x000000,
-      distortionScale: 3.7,
-      fog: false,
-      format: gl.encoding
-    }),
-    [waterNormals]
-  )
-  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
-  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
-}
-
+import Ocean from './ocean';
 
 function Box(props) {
   const mesh = useRef()
--- src/ocean.js
+++ src/ocean.js
@@ -1,48 +1,35 @@
 import React, { useRef, useMemo } from "react";
 import { extend, useThree, useLoader, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
+import waterNormalsJPEG from './waternormals.jpg';
 
 // import { Water } from "three/examples/jsm/objects/Water.js";
 import { Water } from 'three-stdlib'
-
 extend({ Water });
 
 function Ocean() {
-  const ref = useRef();
-  const gl = useThree((state) => state.gl);
-  const waterNormals = useLoader(
-    THREE.TextureLoader, "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/waternormals.jpg"
-  );
-
-
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-  const geom = useMemo(() => new THREE.PlaneGeometry(30000, 30000), []);
+  const ref = useRef()
+  const gl = useThree((state) => state.gl)
+  const waterNormals = useLoader(THREE.TextureLoader, waterNormalsJPEG);
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
   const config = useMemo(
     () => ({
       textureWidth: 512,
       textureHeight: 512,
       waterNormals,
       sunDirection: new THREE.Vector3(),
-      sunColor: 0xeb8934,
-      waterColor: 0x0064b5,
-      distortionScale: 40,
+      sunColor: 0xffffff,
+      // waterColor: 0x001e0f,
+      waterColor: 0x000000,
+      distortionScale: 3.7,
       fog: false,
-      format: gl.encoding,
+      format: gl.encoding
     }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [waterNormals]
-  );
-  useFrame(
-    (state, delta) => (ref.current.material.uniforms.time.value += delta)
-  );
-  return (
-    <water
-      ref={ref}
-      args={[geom, config]}
-      rotation-x={-Math.PI / 2}
-      position={[0, 0, 0]}
-    />
-  );
+  )
+  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
 }
 
 export default Ocean;
