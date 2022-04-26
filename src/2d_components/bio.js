29af71fc9ca855fc74cd6f86b78d7ce2f1565eba
Casey Berman <caseybermanprograms@gmail.com>
2022-04-25 20:00:21 -0400

    added individual webAR things

--- src/2d_components/bio.js
+++ src/2d_components/bio.js
@@ -21,6 +21,9 @@ function Bio() {
 				Casey uses technologies such as Three.js, Unity, React, 8th Wall, Spark AR and many others to make a wide range of colorful, impactful experiences. Some examples of his work:
 				<br/> <br/>
 				<span>Web AR experiences in-store (Xfinity)</span><br/>
+				Entertainment/Peacock Promotion (Zappar)<br />
+				<span>2021 Summer Olympics (8th Wall/Three.js)</span><br/>
+				<span>Sing 2/Happy Holidays promotion (8th Wall/Three.js)</span><br/>
 				Maintenance for all experiences in Xfinity's flagship and premiere stores
 				<br/>
 				Flat Web:
