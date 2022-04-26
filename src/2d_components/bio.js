d3c2c4ff82a82394aabc4a063f51423ad7c418f6
Casey Berman <caseybermanprograms@gmail.com>
2022-04-25 20:29:09 -0400

    added site endpoints and a few other things

--- src/2d_components/bio.js
+++ src/2d_components/bio.js
@@ -5,7 +5,7 @@ function Bio() {
                 casey berman
             </div>
 			<div className='siteBody'>
-                Casey Berman is a creative technologist, developer and musician living in Brooklyn, New York.
+                Casey Berman is a creative technologist, developer and <span>musician</span> living in Brooklyn, New York.
 				Casey currently works at Elephant, a creative company, as a senior Creative Technologist and front-end developer.
 				Working almost exclusively on the Comcast account, Casey has helped push Xfinity's in-store experience into the 
 				21st century using VR, AR and 3D web technologies for over two and a half years. Much of the R&D and prototyping Casey does lays the groundwork
@@ -14,24 +14,30 @@ function Bio() {
 				spends time with his sphynx cat, Phoebe. 
 				<br/> 
             </div>
-			<div className='siteHeader'>
+			<div className='sectionHeader'>
                 work
             </div>
 			<div className="siteBody">
 				Casey uses technologies such as Three.js, Unity, React, 8th Wall, Spark AR and many others to make a wide range of colorful, impactful experiences. Some examples of his work:
 				<br/> <br/>
-				<span>Web AR experiences in-store (Xfinity)</span><br/>
-				Entertainment/Peacock Promotion (Zappar)<br />
-				<span>2021 Summer Olympics (8th Wall/Three.js)</span><br/>
-				<span>Sing 2/Happy Holidays promotion (8th Wall/Three.js)</span><br/>
-				Maintenance for all experiences in Xfinity's flagship and premiere stores
+				<span className="subHeader">Web AR experiences (Xfinity)</span><br/>
+				<a href="https://www.adweek.com/performance-marketing/xfinity-retail-is-putting-shoppers-on-tv-via-augmented-reality-qr-codes/" target="_blank">
+					Entertainment/Peacock Promotion (Zappar)
+				</a>
 				<br/>
-				Flat Web:
+				<span>2021 Summer Olympics (8th Wall/Three.js)</span>
 				<br/>
-				Elephant website (Webby-nominated)
+				<span>Sing 2/Happy Holidays (8th Wall/Three.js)</span><br/>
 				<br/>
-				Marc Jacobs
+				<span className="subHeader">Flat Web:</span>
+				<br/>
+				<a href="https://www.elephant.is" target="_blank">Elephant website (Webby-nominated)</a>
+				<br/>
+				<a href="https://www.marcjacobs.com/" target="_blank">Marc Jacobs</a>
+				<br/>
+				2022 Winter Olympics Promotion
 				<br/> <br/>
+
 			</div>
         </div>
     )
