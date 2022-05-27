736985358fc078a93daf3cdb6d151e26c9b2ebb5
Casey Berman <caseybermanprograms@gmail.com>
2022-05-27 16:28:01 -0400

    minor contentBody cleanup

--- src/2d_components/contentBody.js
+++ src/2d_components/contentBody.js
@@ -2,40 +2,8 @@ import React, {useEffect, useState, useRef } from "react"
 import Bio from "./bio"
 import Work from "./work"
 import Contact from "./contact"
-// class ContentBody extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         // this.state = {
-//         //     selectedSection: 'bio'
-//         // }
-//     }
-//     render() {
-//         let showedSection
-//         // function chooseWhichSectionIsShowed() {
-//             switch(this.props.selectedSection) {
-//                 case  'bio':
-//                     showedSection = <Bio />
-//                     break;
-//                 case 'work':
-//                     showedSection = <Work />
-//                 default:
-//                     showedSection = <Bio />
-//             }
-//         // }
-//         // chooseWhichSectionIsShowed()
-//         return (
-//             <div className='siteBody'>
-//                 {showedSection}
-//             </div>
-//         )
-//     }
-// }
-
 
 function ContentBody(props) {
-    // const overlay = useRef()
-    // const caption = useRef()
-    // const scroll = useRef(0)
     return (
         <div 
             className='siteBody'
