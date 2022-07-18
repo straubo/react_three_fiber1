0cf7160c818aa186eda19f01f9d682f650ba2688
Casey Berman <caseybermanprograms@gmail.com>
2022-07-17 22:31:20 -0400

    fixed bio scroll issue on mobile

--- src/2d_components/bio.js
+++ src/2d_components/bio.js
@@ -3,25 +3,25 @@ function Bio() {
         <div>
 			<div className='sectionHeader'>about</div>
 			<div className='bioInner'>
-				
-			Casey Berman (he/him) is a creative technologist, developer and <span>musician</span> living in Brooklyn, New York. Casey currently works at Elephant, a creative company, as a senior creative technologist and front-end developer.
-				Working almost exclusively on the Comcast account for three years, Casey has helped push Xfinity's in-store experience into the 
-				21st century using VR, AR and 3D web technologies. Much of the R&D and prototyping Casey does lays the groundwork
-				for in-store experiences, whether these be exclusive to premiere and flagship stores, or the 500+ locations nationwide. For more information on Casey's work at Elephant,
-				go to the Work page.
-				{/* click here? */}
-				<br></br><br></br>
-				
-				A professional saxophonist in a past life, Casey attended NYU as a Jazz Studies major. He studied with the likes of 
-				Lee Konitz (of Miles Davis fame), Mark Turner, Bill McHenry, Ben Street, and many more of his heroes. He has toured on three continents 
-				and played at over a hundred venues, including the Kennedy Center, Jazz at Lincoln Center, the Blue Note, the Jazz Gallery and countless others.
-				<br></br><br></br>
-				
-				Casey maintains a rich life of learning and making. When he gets a chance,
-				he rides his bike around the five boroughs, composes in Ableton, breaks things in TouchDesigner, and
-				spends time with his sphynx cat, Phoebe. 
-				<br/> 
-            </div>
+				<div className='bioBody'>
+					Casey Berman (he/him) is a creative technologist, developer and <span>musician</span> living in Brooklyn, New York. Casey currently works at Elephant, a creative company, as a senior creative technologist and front-end developer.
+					Working almost exclusively on the Comcast Xfinity account for three years, Casey has helped push Xfinity's in-store experience into the 
+					21st century using VR, AR and 3D web technologies. Much of the R&D and prototyping Casey does lays the groundwork
+					for in-store experiences, whether these be exclusive to premiere and flagship stores, or the 500+ locations nationwide. For more information on Casey's work at Elephant,
+					go to the Work page.
+					{/* click here? */}
+					<br></br><br></br>
+					
+					A professional saxophonist in a past life, Casey attended NYU as a Jazz Studies major. He studied with the likes of 
+					Lee Konitz (of Miles Davis fame), Mark Turner, Bill McHenry, Ben Street, and many more of his heroes. He has toured on three continents 
+					and played at over a hundred venues, including the Kennedy Center, Jazz at Lincoln Center, the Blue Note, the Jazz Gallery and countless others.
+					<br></br><br></br>
+					
+					Casey maintains a rich life of learning and making. When he gets a chance,
+					he rides his bike around the five boroughs, composes in Ableton, breaks things in TouchDesigner, and
+					spends time with his cat, Phoebe. 
+				</div>
+			</div>
         </div>
     )
 }
