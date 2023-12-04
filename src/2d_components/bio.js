ff91fdf982c277ba71dbb4ec2aed14e87ba4c1bd
Casey Berman <caseybermanprograms@gmail.com>
2023-12-04 16:43:25 -0500

    spacing adjustment on meshcontainer.js, adjusted copy for bio.js

--- src/2d_components/bio.js
+++ src/2d_components/bio.js
@@ -1,12 +1,21 @@
 function Bio() {
-    return (
-        <div>
+	return (
+		<div>
 			<div className='sectionHeader'>about</div>
 			<div className='bioInner'>
 				<div className='bioBody'>
-					Casey Berman (he/him) is a senior webXR developer, creative technologist and <span>musician</span> living in Brooklyn, New York. Casey currently works at Aircards, an agency that specializes in 3D and webXR, as a senior webXR developer.
-					At Aircards, Casey has worked with worldwide brands such as Durex, Johnnie Walker, Comcast, The British Royal Navy, and many more.
+					Casey Berman (he/him) is a senior webXR developer, creative technologist and <span>musician</span> living in Brooklyn, New York.
+					Casey has worked at Aircards, Elephant, Bonzai Media/Metroclick, Saatchi & Saatchi, and as a freelancer.
+
+					<br></br><br></br>
+
+					At Aircards, Casey worked with worldwide brands such as Durex, Johnnie Walker, Comcast, The British Royal Navy, and many more.
 					He has helped push webXR into more people's hands and eyes, and has created experiences that are both beautiful and functional.
+
+					<br></br><br></br>
+					
+					At Elephant, Casey worked with a team to bring the Webby-nominated Elephant website to life using React, Sass and a number of other techs.
+					He was a dedicated developer for Comcast, but additionally worked on projects for Marc Jacobs, and others.
 
 					<br></br><br></br>
 
@@ -16,12 +25,12 @@ function Bio() {
 					<br></br><br></br>
 					
 					Casey maintains a rich life of learning and making. When he gets a chance,
-					he rides his bike around the five boroughs, composes in Ableton, breaks things in TouchDesigner, and
-					spends time with his cat, Phoebe. 
+					he rides his bike around the five boroughs, composes in Ableton, makes things move in TouchDesigner, and
+					spends time with his cats, Phoebe and Zelda.
 				</div>
 			</div>
-        </div>
-    )
+		</div>
+	)
 }
 
 export default Bio;
--- src/3d_components/meshContainer.js
+++ src/3d_components/meshContainer.js
@@ -16,116 +16,117 @@ import posz from '../3d_assets/nightSkyBox/posz.jpg'
 
 
 function Caption(props) {
-    return (
-      <Text
-        position={
-            props.activeItem == null ?
-            [0, 30, -5] : 
-            [0, -20, -75] 
-        }
-        color={
-            props.activeItem == null || 
-            props.activeItem === 'about' ? 
-            '#ffffff' : props.activeItem === 'work' ?
-            '#000000' : '#001e0f'
-        }
-        lineHeight={0.8}
-        font="/Ki-Medium.ttf"
-        fontSize={props.width / 8}
-        material-toneMapped={false}
-        anchorX="center"
-        anchorY="middle"
-        >
-        {props.children}
-      </Text>
-    )
+  return (
+    <Text
+      position={
+        props.activeItem == null ?
+        [0, 30, -5] : 
+        [0, -20, -75] 
+      }
+      color={
+        props.activeItem == null || 
+        props.activeItem === 'about' ? 
+        '#ffffff' : props.activeItem === 'work' ?
+        '#000000' : '#001e0f'
+      }
+      lineHeight={0.8}
+      font="/Ki-Medium.ttf"
+      fontSize={props.width / 8}
+      material-toneMapped={false}
+      anchorX="center"
+      anchorY="middle"
+    >
+      {props.children}
+    </Text>
+  )
 }
   
 
 function MeshContainer(props) {
-    const v = new THREE.Vector3()
-    const [mobile, setMobile] = useState(false)
-    const { width } = useThree((state) => state.viewport)
-    
-    useEffect(() => {
-        width < 71.9 ? setMobile(true) : setMobile(false)        
-    }, [width])
-    
-    useFrame((state) => {
-        // if (props.currentObject === "about") {
-        //     state.camera.position.lerp(v.set(0, 175, 150), 0.04)
-        // } else {
-            state.camera.position.lerp(v.set(0, 10, 80), 0.04)
-        // }
-        state.camera.lookAt(0, 0, 0)
-    })
+  const v = new THREE.Vector3()
+  const [mobile, setMobile] = useState(false)
+  const { width } = useThree((state) => state.viewport)
+  
+  useEffect(() => {
+      width < 71.9 ? setMobile(true) : setMobile(false)        
+  }, [width])
+  
+  useFrame((state) => {
+    // if (props.currentObject === "about") {
+    //     state.camera.position.lerp(v.set(0, 175, 150), 0.04)
+    // } else {
+    state.camera.position.lerp(v.set(0, 10, 80), 0.04)
+    // }
+    state.camera.lookAt(0, 0, 0)
+  })
 
-    return (<>
-        <Caption
-            children={`casey berman`}
-            activeItem={props.currentObject}
-            width={width}
-            mobile={mobile}
-        />
-        {/* mesh colors:
-        green: 428F70
-        deep blue(not navy but darker): 313D6B
-        lighter blue (cyrulean?): 3E6E90
-        lighter cyrulean: 679DAE
-        "brighter" blue: 3957A5
-        orange (slightly ugly, reflective): D46C40
-        flesh-ish (not reflective): DDBF99
-        deep grey: 3A3B3B
-        bright yellow (n-r):F2BB1D
-        cream: EADBC0
-         */}
-        <ModelLoader 
-            scale={0.08} 
-            modelName={'human'} 
-            modelExtension={'BaseMesh_Man_Simple'}
-            // position={[-35, 3, 0]}
-            position={[
-                -width/3.6, 3, 0]}
-            selectObj={props.selectObj}
-            activeItem={props.currentObject}
-            section={'about'}
-            color={'#3E6E90'}
-            // selectedColor={'pink'}
-            selectedColor={'#1f3e2c'}
-        />
-        <ModelLoader
-            scale={3}
-            modelName={'headset'} 
-            modelExtension={'VR_simple'}
-            position={[0, 7, 0]}
-            selectObj={props.selectObj}
-            activeItem={props.currentObject}
-            section={'work'}
-            color={'#679DAE'}
-            selectedColor={'#000000'}
-        />
-        <LaptopLoader
-            activeItem={props.currentObject}
-            selectObj={props.selectObj}
-            width={width/3.6}
-        />
-        <Skybox 
-            activeItem={props.currentObject}
-        />
+  return (
+    <>
+      <Caption
+          children={`casey berman`}
+          activeItem={props.currentObject}
+          width={width}
+          mobile={mobile}
+      />
+      {/* mesh colors:
+      green: 428F70
+      deep blue(not navy but darker): 313D6B
+      lighter blue (cyrulean?): 3E6E90
+      lighter cyrulean: 679DAE
+      "brighter" blue: 3957A5
+      orange (slightly ugly, reflective): D46C40
+      flesh-ish (not reflective): DDBF99
+      deep grey: 3A3B3B
+      bright yellow (n-r):F2BB1D
+      cream: EADBC0
+        */}
+      <ModelLoader
+          scale={0.08} 
+          modelName={'human'} 
+          modelExtension={'BaseMesh_Man_Simple'}
+          // position={[-35, 3, 0]}
+          position={[-width/3.6, 3, 0]}
+          selectObj={props.selectObj}
+          activeItem={props.currentObject}
+          section={'about'}
+          color={'#3E6E90'}
+          // selectedColor={'pink'}
+          selectedColor={'#1f3e2c'}
+      />
+      <ModelLoader
+        scale={3}
+        modelName={'headset'} 
+        modelExtension={'VR_simple'}
+        position={[0, 7, 0]}
+        selectObj={props.selectObj}
+        activeItem={props.currentObject}
+        section={'work'}
+        color={'#679DAE'}
+        selectedColor={'#000000'}
+      />
+      <LaptopLoader
+        activeItem={props.currentObject}
+        selectObj={props.selectObj}
+        width={width/3.6}
+      />
+      <Skybox 
+        activeItem={props.currentObject}
+      />
 
-        <Menu3D
-            selectObj={props.selectObj}
-            activeItem={props.currentObject}
-            width={width}
-            mobile={mobile}
-        />
-        {/* <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} intensity={0.2}/> */}
-        {/* <Environment preset="night" /> */}
-        <Environment files={[posx, negx, posy, negy, posz, negz]} /> 
-        <Noodles 
-            activeItem={props.currentObject}
-        />
-    </>)
+      <Menu3D
+        selectObj={props.selectObj}
+        activeItem={props.currentObject}
+        width={width}
+        mobile={mobile}
+      />
+      {/* <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} intensity={0.2}/> */}
+      {/* <Environment preset="night" /> */}
+      <Environment files={[posx, negx, posy, negy, posz, negz]} />
+      <Noodles
+        activeItem={props.currentObject}
+      />
+    </>
+  )
 }
 
 export default MeshContainer
